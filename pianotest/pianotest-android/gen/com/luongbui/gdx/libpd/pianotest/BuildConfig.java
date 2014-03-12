bc933cc133458c5238e48399e796c885031a9798
luong <manh.luong@gmail.com>
2014-03-12 03:47:23 +0100

    First commit.

---
+++ pianotest/pianotest-android/gen/com/luongbui/gdx/libpd/pianotest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.luongbui.gdx.libpd.pianotest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ pianotest/pianotest-desktop/src/com/luongbui/gdx/libpd/pianotest/Main.java
@@ -0,0 +1,15 @@
+package com.luongbui.gdx.libpd.pianotest;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "pianotest";
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new PianoTest(), cfg);
+	}
+}
