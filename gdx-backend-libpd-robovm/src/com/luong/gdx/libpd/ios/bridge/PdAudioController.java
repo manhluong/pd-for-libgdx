18060db738f55bea02b834ddb70971d277c28050
luong <manh.luong@gmail.com>
2014-03-19 04:06:22 +0100

    RoboVM bridge via Bro.

---
+++ gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/bridge/PdAudioController.java
@@ -0,0 +1,20 @@
+package com.luong.gdx.libpd.ios.bridge;
+
+import org.robovm.cocoatouch.foundation.NSObject;
+import org.robovm.objc.ObjCRuntime;
+import org.robovm.objc.annotation.NativeClass;
+import org.robovm.rt.bro.annotation.Library;
+
+@Library(Library.INTERNAL)
+@NativeClass
+public class PdAudioController extends NSObject {
+
+	static {
+		ObjCRuntime.bind(PdAudioController.class);
+		}
+	
+	public PdAudioController() {
+		
+		}
+	
+	}
