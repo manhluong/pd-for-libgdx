81f68ab42193c53b8b1ff719cde39ee01f7bed70
luong <manh.luong@gmail.com>
2014-03-21 09:47:52 +0100

    Initizla PDAudioControllerBridge.

--- gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/bridge/PdAudioController.java
+++ gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/bridge/PdAudioController.java
@@ -2,6 +2,7 @@ package com.luong.gdx.libpd.ios.bridge;
 
 import org.robovm.cocoatouch.foundation.NSObject;
 import org.robovm.objc.ObjCRuntime;
+import org.robovm.objc.Selector;
 import org.robovm.objc.annotation.NativeClass;
 import org.robovm.rt.bro.annotation.Bridge;
 import org.robovm.rt.bro.annotation.Library;
@@ -15,11 +16,27 @@ public class PdAudioController extends NSObject {
 		ObjCRuntime.bind(PdAudioController.class);
 		}
 	
+	private final Selector configureAmbientWithSampleRate$ = Selector.register("configureAmbientWithSampleRate:numberChannels:mixingEnabled:");
+	
 	public PdAudioController() {
 		init();
+		}
+	
+	public PdAudioStatus configureAmbientWithSampleRate(int sampleRate, int numChannels, boolean mixingEnabled) {
+		return objc_configureAmbientWithSampleRate(this,
+													configureAmbientWithSampleRate$,
+													sampleRate,
+													numChannels,
+													mixingEnabled);
 		}
 	
 	@Bridge
 	private native @Pointer long init();
 	
+	@Bridge
+	private native PdAudioStatus objc_configureAmbientWithSampleRate(PdAudioController __self__,
+																		Selector __cmd__,
+																		int sampleRate,
+																		int numChannels,
+																		boolean mixingEnabled);
 	}
