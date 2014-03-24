2c967858ab221923f5c629fadc0d18874d4e8828
luong <manh.luong@gmail.com>
2014-03-24 23:03:39 +0100

    GdxPDiOS has some comments as guidance

--- gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/GdxPDIOS.java
+++ gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/GdxPDIOS.java
@@ -14,39 +14,52 @@ public class GdxPDiOS implements GdxPD {
 	@Override
 	public void init() {
 		// TODO
+		//_audioController = [[PdAudioController alloc] init];
+		//if ([self.audioController configureAmbientWithSampleRate:44100
+		//     numberChannels:2 mixingEnabled:YES] != PdAudioOK) {
+		//	NSLog(@"failed to initialize audio components");
+		//	}
 		}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		//[PdBase closeFile:patch];
+		//[PdBase setDelegate:nil];
 		}
 
 	@Override
 	public void loadPatch(String patchName) {
 		// TODO Auto-generated method stub
-		
+		//dispatcher = [[PdDispatcher alloc] init];
+		//[PdBase setDelegate:dispatcher];
+		//patch = [PdBase openFile:@"tuner.pd"
+		//path:[[NSBundle mainBundle] resourcePath]];
+		//if (!patch) {
+		//	NSLog(@"Failed to open patch!");
+		//	}
 		}
 
 	@Override
 	public void startAudio() {
 		// TODO Auto-generated method stub
-		
+		// self.audioController.active = YES;
 		}
 
 	@Override
 	public void stopAudio() {
 		// TODO Auto-generated method stub
-		
+		// self.audioController.active = NO;
 		}
 
 	@Override
 	public void sendBang(String bang) {
 		// TODO Auto-generated method stub
-		
+		//[PdBase sendBangToReceiver:@"trigger"];
 		}
 
 	public void sendFloat(String label, float number) {
 		// TODO
+		//[PdBase sendFloat:n toReceiver:@"midinote"];
 		}
 	}
