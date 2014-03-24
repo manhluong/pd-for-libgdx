2a903a86cf000781e2224417d07304473dfbadde
luong <manh.luong@gmail.com>
2014-03-24 21:39:01 +0100

    used new bindings

--- gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/bridge/PdBase.java
+++ gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/bridge/PdBase.java
@@ -2,9 +2,8 @@ package com.luong.gdx.libpd.ios.bridge;
 
 import org.robovm.cocoatouch.foundation.NSObject;
 import org.robovm.objc.ObjCRuntime;
-import org.robovm.objc.Selector;
+import org.robovm.objc.annotation.Method;
 import org.robovm.objc.annotation.NativeClass;
-import org.robovm.rt.bro.annotation.Bridge;
 import org.robovm.rt.bro.annotation.Library;
 
 @Library(Library.INTERNAL)
@@ -16,18 +15,15 @@ public class PdBase extends NSObject {
 		}
 	
 	public PdBase() {
-		objc_initialize(this, initialize);
+		super((SkipInit)null);
+		initialize();
 		}
 	
 	/**
-	 * @method
-	 * 
 	 * + (void)initialize;
 	 */
-	private static final Selector initialize = Selector.register("initialize");
-	
-	@Bridge
-	private native static void objc_initialize (PdBase __self__, Selector __cmd__);
+	@Method(selector = "initialize")
+	public static native void initialize();
 	
 	/**
 	 * PdBase retains the delegate: call setDelegate with nil in order to release delegate.
