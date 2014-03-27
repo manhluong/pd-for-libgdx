ae479e2bf869ac4aea0fe29d0196780095525a33
luong <manh.luong@gmail.com>
2014-03-27 02:36:00 +0100

    added native add / remove methods

--- gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/bindings/PdDispatcher.java
+++ gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/bindings/PdDispatcher.java
@@ -8,6 +8,9 @@ import org.robovm.objc.annotation.NativeClass;
 import org.robovm.rt.bro.annotation.Library;
 import org.robovm.rt.bro.annotation.Pointer;
 
+/**
+ * @ interface PdDispatcher : NSObject<PdReceiverDelegate>
+ */
 @Library(Library.INTERNAL)
 @NativeClass
 public class PdDispatcher extends NSObject implements PdReceiverDelegate {
@@ -23,6 +26,25 @@ public class PdDispatcher extends NSObject implements PdReceiverDelegate {
 	
 	@Method(selector = "init")
 	public native @Pointer long init();
+	
+	/**
+	 * // Adds a listener for the given source (i.e., send symbol) in Pd.  If this is the first<br>
+	 * // listener for this source, a subscription for this symbol will automatically be registered<br>
+	 * // with PdBase.<br>
+	 * <br>
+	 * - (int)addListener:(NSObject<PdListener> *)listener forSource:(NSString *)source;
+	 */
+	@Method(selector = "addListener:forSource:")
+	public native int addListener(PdListener listener, NSString source);
+	
+	/**
+	 * // Removes a listener for a source symbol and unsubscribes from messages to this symbol if<br>
+	 * // the listener was the last listener for this symbol.<br>
+	 * <br>
+	 * - (int)removeListener:(NSObject<PdListener> *)listener forSource:(NSString *)source;<br>
+	 */
+	@Method(selector = "removeListener:forSource:")
+	public native int removeListener(PdListener listener, NSString source);
 
 	@Override
 	public void receivePrint(NSString message) {
@@ -34,8 +56,7 @@ public class PdDispatcher extends NSObject implements PdReceiverDelegate {
 	 * // No need to synchronize here.<br>
 	 * - (void)receivePrint:(NSString *)message<br>
 	 * <br>
-	 * You should override the non-native one.<br>
-	 * <br>
+	 * <b>You should override the non-native one.</b><br>
 	 * @param message
 	 */
 	@Method(selector = "receivePrint:")
