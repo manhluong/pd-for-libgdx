87617d785dfda6af2da82b0f91c9c4ce3bfa0375
luong <manh.luong@gmail.com>
2014-03-28 18:34:18 +0100

    renamed

--- gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/GdxPDiOSbis.java
+++ gdx-backend-libpd-robovm/src/com/luong/gdx/libpd/ios/GdxPDiOSbis.java
@@ -17,7 +17,7 @@ import com.luong.gdx.libpd.ios.bindings.PdBase;
 import com.luong.gdx.libpd.ios.bindings.PdDispatcher;
 import com.luong.gdx.libpd.ios.bindings.PdListener;
 
-public class GdxPDiOS implements GdxPD {
+public class GdxPDiOSbis implements GdxPD {
 	
 	protected static PdAudioController audioController;
 	
@@ -33,7 +33,7 @@ public class GdxPDiOS implements GdxPD {
 	
 	protected static boolean mixingEnabled;
 	
-	public GdxPDiOS() {
+	public GdxPDiOSbis() {
 		sampleRate = 44100;
 		useInChannels = false;
 		numChannels = 2;
@@ -42,7 +42,7 @@ public class GdxPDiOS implements GdxPD {
 		PdBase.setDelegate(dispatcher);
 		}
 	
-	public GdxPDiOS(int rate, boolean inChannels, int outChannels, boolean mixing) {
+	public GdxPDiOSbis(int rate, boolean inChannels, int outChannels, boolean mixing) {
 		this();
 		sampleRate = rate;
 		useInChannels = inChannels;
