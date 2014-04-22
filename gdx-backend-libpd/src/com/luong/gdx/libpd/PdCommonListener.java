30f82cbb2480ba0ba3ac2e0116cd5ae1d7aa7152
luong <manh.luong@gmail.com>
2014-04-22 12:03:45 +0200

    Code beautify.

--- gdx-backend-libpd/src/com/luong/gdx/libpd/PdCommonListener.java
+++ gdx-backend-libpd/src/com/luong/gdx/libpd/PdCommonListener.java
@@ -17,7 +17,7 @@
 package com.luong.gdx.libpd;
 
 /**
- * A dummy interface so we can have a common addListener() and removeListener methods.<br>
+ * A dummy interface so we can have a common addListener() and removeListener() methods.<br>
  * <br>
  * <b>All PdListeners most implement this interface too.</b>
  */
